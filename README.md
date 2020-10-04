ebedf5c79ab0f083f7100ef06cc04a3995beb11b
Heather Ackerman <hla20115213@gmail.com>
2020-10-04 18:00:43 -0500

    Worked with my tutor, Sammantha, to finsh the js and make this functional. I also updated the readme.

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # Password_Generator
-I was given html, css, and some beginning js. My goal was to create a password generator that would produce a random password between 8 and 128 characters. I was to use prompts to ask how many characters the password should be, and if the password was to have special characters, numbers, lowercase or uppercase letters.
-
-I developed a list of prompts and tried to push the answers into a new array that the password would be randomly generated from. I unfortunatley did not have enough time to perfect it and get it functional before the due date, but I will continue to try. 
+I was given html, css, and some beginning js. My goal was to create a password generator that would produce a random password between 8 and 128 characters. The user decides how many characters the password should be, and if the password will include special characters, numbers, lowercase, or uppercase letters.
 
 ## Links
-Will put links here when completed. 
+https://heatherackerman.github.io/Password_Generator/
+https://github.com/HeatherAckerman/Password_Generator
 
 ## Usage Instructions
-Not yet functional. 
+Click the "Generate Password" button and answer all of the promps on the screen. A random password meeting your specifications will then be displayed. 
 
 ## Installation Instructions
 N/A
--- script.js
+++ script.js
@@ -17,47 +17,91 @@ generateBtn.addEventListener("click", writePassword);
 //Variables to choose from. The newArray is where the chosen variables will go
 var specialChar = [",", "!", "@", "#", "$", "%", "-", "&", "-", "=", "_"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p",];
-var upperCase = ["Q","W","E","R","T", "y", "U", "I", "o", "P"];
-var newArray = [];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 
 
 function generatePassword() {
-//Prompts are asking what variables the password should include then they are being pushed to the newArray
+  var options = getPasswordParameters();
+  var newArray = [];
+  var result = [];
+  var possibleChar = [];
 
-var passLength = parseInt(prompt("How many characters (8-128) would you like your password to be?"));
-if (passLength < 7 || passLength > 129); {
-// alert("Please keep it between " + 8 + " and " + 128) *alert was showing everytime* 
-}
-alert(passLength + " characters included")
+  if (options.hasspecialChar === true) {
+    possibleChar = possibleChar.concat(specialChar);
+    
+    newArray.push(getRandom(specialChar));
 
-var prompt1 = prompt("Do you want to include special characters? y or n")
-if (prompt1 === "y") {
-  alert("Special characters included!");
-  newArray.push(specialChar);
+  }
+  if (options.hasnumbers === true) {
+    possibleChar = possibleChar.concat(number);
+    
+    newArray.push(getRandom(number));
+  }
+  if (options.haslowerCase === true) {
+    possibleChar = possibleChar.concat(lowerCase);
+    
+    newArray.push(getRandom(lowerCase));
+  }
+  if (options.hasupperCase === true) {
+    possibleChar = possibleChar.concat(upperCase);
+    
+    newArray.push(getRandom(upperCase));
+  }
+
+
+
+  for (var i = 0; i < options.passLength; i++) {
+    var possibleCharacter = getRandom(possibleChar)
+    result.push(possibleCharacter)
+    
+  }
+
+  for (var i = 0; i < newArray.length; i++) {
+    
+    result[i] = newArray[i]
+  }
+  
+console.log(result)
+  return result.join("")
 }
 
-var prompt2 = prompt("Do you want to include numbers? y or n")
-if (prompt2 === "y") {
-  alert("Numbers included!");
-  newArray.push(number);
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
 }
 
-var prompt3 = prompt("Do you want to include lowercase letters? y or n")
-if (prompt3 === "y") {
-  alert("Lowercase letters included!");
-  newArray.push(lowerCase);
+function getPasswordParameters() {
+  //Prompts are asking what variables the password should include then they are being pushed to the newArray
+
+  var passLength = parseInt(prompt("How many characters (8-128) would you like your password to be?"));
+
+  if (passLength < 7 || passLength > 129); {
+    // alert("Please keep it between " + 8 + " and " + 128) *alert was showing everytime* 
+  }
+  alert(passLength + " characters included")
+
+  var hasspecialChar = confirm("Do you want to include special characters? y or n")
+ 
+
+  var hasnumbers = confirm("Do you want to include numbers? y or n")
+ 
+
+  var haslowerCase = confirm("Do you want to include lowercase letters? y or n")
+ 
+
+  var hasupperCase = confirm("Do you want to include uppercase letters? y or n")
+ 
+  var passwordOptions = {
+    passLength: passLength,
+    hasspecialChar: hasspecialChar,
+    hasnumbers: hasnumbers,
+    haslowerCase: haslowerCase,
+    hasupperCase: hasupperCase,
+  }
+  return passwordOptions;
 }
 
-var prompt4 = prompt("Do you want to include uppercase letters? y or n")
-if (prompt4 === "y") {
-  alert("Uppercase letters included!");
-  newArray.push(upperCase);
-}
-//For loop not working. I know it is bad practice to turn in half completed code, but time has simply run out and something is better than nothing
-  for (var i = 0; i < passLength; i++) {
-    //I was hoping the code below would take strings from the new array and scramble them into the password that would show in the html
-   return newArray.charAt(Math.floor(Math.random()));
-   
-}
-}
